ab158e0cf50694e9432df8b3d45c6547fe7e001f
Esteban Vazquez <mestebanve123@gmail.com>
2024-02-07 06:57:18 -0800

    Se agrego la credencializacion a git

---
+++ src/main/java/com/mycompany/escuela/Escuela.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.escuela;
+
+/**
+ *
+ * @author arman
+ */
+public class Escuela {
+    
+    public static void main(String[] args) {
+        Registro r = new Registro(); 
+        r.setVisible(true);
+    }
+}
